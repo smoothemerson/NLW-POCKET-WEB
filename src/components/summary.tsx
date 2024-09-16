a720f83e3c4e99d3a861213b1905b046f9ec27b0
emerson <emerson8096@gmail.com>
2024-09-16 09:39:42 -0300

    fix: add if goalsperday exists then continues

--- src/components/summary.tsx
+++ src/components/summary.tsx
@@ -66,40 +66,43 @@ export function Summary() {
 
       <PendingGoals />
 
-      {Object.entries(data.goalsPerDay).map(([date, goals]) => {
-        const weekDay = dayjs(date).format("dddd");
-        const formattedDate = dayjs(date).format("D [de] MMMM");
+      {data?.goalsPerDay &&
+        Object.entries(data.goalsPerDay).map(([date, goals]) => {
+          const weekDay = dayjs(date).format("dddd");
+          const formattedDate = dayjs(date).format("D [de] MMMM");
 
-        return (
-          <div key={date} className="flex flex-col gap-6">
-            <h2 className="text-xl font-medium">Sua semana</h2>
+          return (
+            <div key={date} className="flex flex-col gap-6">
+              <h2 className="text-xl font-medium">Sua semana</h2>
 
-            <div className="flex flex-col gap-4">
-              <h3 className="font-medium">
-                <span className="capitalize">{weekDay}</span>{" "}
-                <span className="text-zinc-400 text-xs">({formattedDate})</span>
-              </h3>
+              <div className="flex flex-col gap-4">
+                <h3 className="font-medium">
+                  <span className="capitalize">{weekDay}</span>{" "}
+                  <span className="text-zinc-400 text-xs">
+                    ({formattedDate})
+                  </span>
+                </h3>
 
-              <ul className="flex flex-col gap-3">
-                {goals.map((goal) => {
-                  const time = dayjs(goal.completedAt).format("HH:mm");
+                <ul className="flex flex-col gap-3">
+                  {goals.map((goal) => {
+                    const time = dayjs(goal.completedAt).format("HH:mm");
 
-                  return (
-                    <li key={goal.id} className="flex items-center gap-2">
-                      <CheckCircle2 className="size-4 text-pink-500" />
-                      <span className="text-sm text-zinc-400">
-                        Você completou{" "}
-                        <span className="text-zinc-100">{goal.title}</span> às{" "}
-                        <span className="text-zinc-100">{time}</span>
-                      </span>
-                    </li>
-                  );
-                })}
-              </ul>
+                    return (
+                      <li key={goal.id} className="flex items-center gap-2">
+                        <CheckCircle2 className="size-4 text-pink-500" />
+                        <span className="text-sm text-zinc-400">
+                          Você completou{" "}
+                          <span className="text-zinc-100">{goal.title}</span> às{" "}
+                          <span className="text-zinc-100">{time}</span>
+                        </span>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
     </div>
   );
 }
